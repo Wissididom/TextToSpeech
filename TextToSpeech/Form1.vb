4d6ece66e86729ab5b1e316bc7e86c7d555ad796
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-03-01 00:27:07 +0100

    Update to .NET 10

--- TextToSpeech/Form1.vb
+++ TextToSpeech/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports System.Windows.Forms
+
+Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Me.Text = Application.ProductName & " - bereit"
